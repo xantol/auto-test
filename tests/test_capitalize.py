2087e7e147c01564e1d32f0bcf01fe20f9716de5
xantol <xantol@rambler.ru>
2024-06-25 20:19:44 +0500

    update test

--- tests/test_capitalize.py
+++ tests/test_capitalize.py
@@ -1,9 +1,7 @@
 from capitalize import capitalize
 
-if capitalize('hello') != 'Hello':
-    raise Exception('Функция работает неверно!')
+assert capitalize('hello') == 'Hello'
 
-if capitalize('') != '':
-    raise Exception('Функция работает неверно!')
+assert capitalize('') == ''
 
 print('Все тесты пройдены!')
